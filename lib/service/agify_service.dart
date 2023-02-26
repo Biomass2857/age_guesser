fbd1744a45df06b216726d4b9388d6d088fb6c4b
Biomass2857 <wasserfrosch1234@gmail.com>
2023-02-26 16:24:30 +0100

    add agify api service

---
+++ lib/service/agify_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:async';
+import 'package:age_guesser/service/agify_result.dart';
+import 'package:http/http.dart' as http;
+
+class AgifyApiService {
+  Future<AgifyResult> getAgeForName(String name) {
+    var httpClient = http.Client();
+    var url = _getRequestURLForName(name);
+    return httpClient.get(url).then((response) {
+      var json = jsonDecode(response.body);
+      return AgifyResult.fromJSON(json);
+    });
+  }
+
+  Uri _getRequestURLForName(String name) {
+    return Uri(
+        scheme: 'https',
+        host: 'api.agify.io',
+        path: '/',
+        queryParameters: {'name': name});
+  }
+}
